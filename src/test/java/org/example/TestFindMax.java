cb98084e7226edaf01896aa39f31fa9342fe9354
Kuldeep147 <kuldeep.bargurjar@gmail.com>
2022-12-29 12:48:33 +0530

    Adding uc1

--- src/test/java/org/example/TestFindMax.java
+++ src/test/java/org/example/TestFindMax.java
@@ -2,4 +2,10 @@ package org.example;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 public class TestFindMax {
+    @Test
+    public void testFindMax(){
+        assertEquals(3,FindMax.findMax(new Integer[] {3,1,2}));
+        assertEquals(5,FindMax.findMax(new Integer[] {-6,5,-90}));
+        assertEquals(3,FindMax.findMax(new Integer[] {1,2,3}));
+    }
 }
