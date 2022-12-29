9da51f6ab956d0983db3cc9a5676862919164bd8
Kuldeep147 <kuldeep.bargurjar@gmail.com>
2022-12-29 15:47:31 +0530

    Adding uc5

--- src/main/java/org/example/FindMax.java
+++ src/main/java/org/example/FindMax.java
@@ -9,16 +9,19 @@ public class FindMax {
     public static Integer findMax(Integer[] arr){
         Arrays.sort(arr);
         Integer max = arr[arr.length - 1];
+        System.out.println(max);
         return max;
     }
     public static Float findMaxFloat(Float[] arr){
         Arrays.sort(arr);
         Float max = arr[arr.length - 1];
+        System.out.println(max);
         return max;
     }
     public static String findMaxString(String[] arr){
         Arrays.sort(arr);
         String max = arr[arr.length - 1];
+        System.out.println(max);
         return max;
     }
 }
